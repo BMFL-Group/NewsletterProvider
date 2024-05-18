06b28e82dcc64fb9cea042706edfccf85f682662
Martin <martinkristensen12@gmail.com>
2024-05-18 07:30:12 +0200

    Add project files.

---
+++ Data/Data/Contexts/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Data.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Data.Contexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<SubscribeEntity> Subscribers { get; set; }
+}
