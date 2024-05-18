a5e4af6ad12e6770c69091aeeb98ba4a938cd694
Martin <martinkristensen12@gmail.com>
2024-05-18 10:01:54 +0200

    redone constructor

--- NewsletterProvider/Functions/Unsubscribe.cs
+++ NewsletterProvider/Functions/Unsubscribe.cs
@@ -7,33 +7,32 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace NewsletterProvider.Functions
-{
-    public class Unsubscribe(ILogger<Unsubscribe> logger, DataContext context)
-    {
-        private readonly ILogger<Unsubscribe> _logger = logger;
-        private readonly DataContext _context = context;
+namespace NewsletterProvider.Functions;
 
-        [Function("Unsubscribe")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+public class Unsubscribe(ILogger<Unsubscribe> logger, DataContext context)
+{
+    private readonly ILogger<Unsubscribe> _logger = logger;
+    private readonly DataContext _context = context;
+
+    [Function("Unsubscribe")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
+        var body = await new StreamReader(req.Body).ReadToEndAsync();
+        if (!string.IsNullOrEmpty(body))
         {
-            var body = await new StreamReader(req.Body).ReadToEndAsync();
-            if (!string.IsNullOrEmpty(body))
+            var subscribeEntity = JsonConvert.DeserializeObject<SubscribeEntity>(body);
+            if (subscribeEntity != null)
             {
-                var subscribeEntity = JsonConvert.DeserializeObject<SubscribeEntity>(body);
-                if (subscribeEntity != null)
+                var existingSubscriber = await _context.Subscribers.FirstOrDefaultAsync(x => x.Email == subscribeEntity.Email);
+                if (existingSubscriber != null)
                 {
-                    var existingSubscriber = await _context.Subscribers.FirstOrDefaultAsync(x => x.Email == subscribeEntity.Email);
-                    if (existingSubscriber != null)
-                    {
-                        _context.Remove(existingSubscriber);
-                        await _context.SaveChangesAsync();
-                        return new OkObjectResult(new { Status = 200, Message = "Subscriber was updated. " });
-                    }
+                    _context.Remove(existingSubscriber);
+                    await _context.SaveChangesAsync();
+                    return new OkObjectResult(new { Status = 200, Message = "Subscriber was unsubscribed. " });
                 }
             }
-
-            return new BadRequestObjectResult(new { Status = 400, Message = "Unable to unsubscribe at the moment. " });
         }
+
+        return new BadRequestObjectResult(new { Status = 400, Message = "Unable to unsubscribe at the moment. " });
     }
 }
