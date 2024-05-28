2fa47ab72607ab35a605a055aba74ba991028b7a
Martin <martinkristensen12@gmail.com>
2024-05-28 16:38:39 +0200

    added GetAllSubscribers function

---
+++ NewsletterProvider/Functions/GetAllSubscribers.cs
@@ -0,0 +1,37 @@
+using Data.Data.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace NewsletterProvider.Functions;
+
+public class GetAllSubscribers
+{
+    private readonly ILogger<GetAllSubscribers> _logger;
+    private readonly DataContext _context;
+
+    public GetAllSubscribers(ILogger<GetAllSubscribers> logger, DataContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [Function("GetAllSubscribers")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+    {
+        try
+        {
+            var subscribers = await _context.Subscribers.ToListAsync();
+
+            return new OkObjectResult(subscribers);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while processing the subscription request.");
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+       
+    }
+}
